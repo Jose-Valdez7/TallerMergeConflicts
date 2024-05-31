1f5d7b515d4bdcbd691f1cffc0485d89437999de
josuetipan <josuetipan101@gmail.com>
2024-05-31 14:28:41 -0500

    Productos Add estu1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,19 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenido a mi tienda, soy Josue</Text>
+
+        <View style={styles.proH}>
+            <Text>Helado</Text>
+        </View>
+        <View style={styles.proC}>
+            <Text>Cachito</Text>
+        </View>
+        <View style={styles.proCC}>
+            <Text>Coca-cola</Text>
+        </View>
+        
+
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,8 +28,18 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'pink',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    proH:{
+        backgroundColor:"yellow"
+    },
+    proC:{
+        backgroundColor:"green"
+    },
+    proCC:{
+        backgroundColor:"red"
+    }
+
   });
